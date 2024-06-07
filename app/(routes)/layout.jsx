e76a8aa2da8a51d1a6edfe31a5430e59d12ff8a6
Manish_Syal <manishsyal77@gmail.com>
2024-06-07 14:49:22 +0530

    Created a new rout called budgets and replaced the [...dashboard] to foldername as dashboard and if user haven't created any budget, then redirect it to budgets page

--- app/(routes)/layout.jsx
+++ app/(routes)/layout.jsx
@@ -1,13 +1,36 @@
+"use client";
 import {
   ClerkProvider,
   SignedIn,
   SignedOut,
   RedirectToSignIn,
+  useUser,
 } from "@clerk/nextjs";
-import SideNav from "./[...dashboard]/_components/SideNav";
-import DashboardHeader from "./[...dashboard]/_components/DashboardHeader";
+import SideNav from "./dashboard/_components/SideNav";
+import DashboardHeader from "./dashboard/_components/DashboardHeader";
+import { db } from "@/utils/dbConfig";
+import { Budgets } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function DashboardLayout({ children }) {
+  const { user } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
+  const checkUserBudgets = async () => {
+    const result = await db
+      .select()
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+
+    console.log(result);
+    if (result?.length == 0) {
+      router.replace("/dashboard/budgets");
+    }
+  };
   return (
     <div>
       {/* <ClerkProvider> */}
