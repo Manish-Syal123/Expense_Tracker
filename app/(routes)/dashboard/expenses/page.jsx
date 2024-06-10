ac2232249a4439aeccc0aa9aa16710448eeb1096
Manish_Syal <manishsyal77@gmail.com>
2024-06-10 19:48:21 +0530

    Created Expenses route where all the expenses will be shown

--- app/(routes)/dashboard/expenses/page.jsx
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -1,11 +1,46 @@
 "use client";
-import { useRouter } from "next/navigation";
-import React from "react";
+import { db } from "@/utils/dbConfig";
+import { Budgets, Expenses } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { desc, eq } from "drizzle-orm";
+import React, { useEffect, useState } from "react";
+import ExpenseListTable from "./_components/ExpenseListTable";
 
-const page = () => {
-  const router = useRouter();
-  router.replace("/dashboard/expenses/0");
-  return <div>Expenses</div>;
+const MyExpenses = () => {
+  const [expensesList, setExpenseslist] = useState([]);
+  const { user } = useUser();
+  useEffect(() => {
+    user && getAllExpenses();
+  }, [user]);
+
+  // Getting all the expenses of the current logged In user
+  const getAllExpenses = async () => {
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .orderBy(desc(Expenses.id));
+
+    setExpenseslist(result);
+    // console.log(result);
+  };
+  return (
+    <div>
+      <h2 className="font-bold text-3xl">My All Expenses</h2>
+      <div className="mt-11">
+        <ExpenseListTable
+          expensesList={expensesList}
+          refreshData={() => getAllExpenses()}
+        />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default MyExpenses;
