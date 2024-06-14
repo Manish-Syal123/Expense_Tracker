7a0251a57a2d77eb3bac8acba12c17e19998b4aa
Manish_Syal <manishsyal77@gmail.com>
2024-06-14 14:08:42 +0530

    Sorted expenses based on selected year

--- app/(routes)/dashboard/expenses/page.jsx
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -1,17 +1,33 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import { db } from "@/utils/dbConfig";
 import { Budgets, Expenses } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { desc, eq } from "drizzle-orm";
-import React, { useEffect, useState } from "react";
 import ExpenseListTable from "./_components/ExpenseListTable";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const MyExpenses = () => {
   const [expensesList, setExpenseslist] = useState([]);
+  const [filteredExpensesList, setFilteredExpensesList] = useState([]);
+  const [selectedYear, setSelectedYear] = useState(null);
   const { user } = useUser();
+
   useEffect(() => {
     user && getAllExpenses();
   }, [user]);
+
+  useEffect(() => {
+    if (selectedYear !== null) {
+      const filtered = expensesList.filter(
+        (expense) => new Date(expense.createdAt).getFullYear() === selectedYear
+      );
+      setFilteredExpensesList(filtered);
+    } else {
+      setFilteredExpensesList(expensesList);
+    }
+  }, [expensesList, selectedYear]);
 
   // Getting all the expenses of the current logged In user
   const getAllExpenses = async () => {
@@ -28,14 +44,35 @@ const MyExpenses = () => {
       .orderBy(desc(Expenses.id));
 
     setExpenseslist(result);
-    // console.log(result);
   };
+
+  const handleYearChange = (date) => {
+    if (date) {
+      setSelectedYear(date.getFullYear());
+    } else {
+      setSelectedYear(null);
+    }
+  };
+
   return (
     <div>
       <h2 className="font-bold text-3xl">My All Expenses</h2>
+      <div className="mt-4">
+        <DatePicker
+          selected={selectedYear ? new Date(selectedYear, 0) : null}
+          onChange={handleYearChange}
+          showYearPicker
+          dateFormat="yyyy"
+          className="border p-2"
+          placeholderText="Select Year"
+          showPopperArrow={false}
+          customInput={<input type="text" className="border p-2" readOnly />}
+          isClearable
+        />
+      </div>
       <div className="mt-11">
         <ExpenseListTable
-          expensesList={expensesList}
+          expensesList={filteredExpensesList}
           refreshData={() => getAllExpenses()}
         />
       </div>
